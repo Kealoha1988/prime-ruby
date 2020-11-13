bde85835c5463ce5df90eb101909868af2e78418
Kealoha1988 <kealohaabe1988@gmail.com>
2020-11-13 05:32:55 -0500

    commit tree ca8b5ed762d14aa621c68c00d295db71a5ccd493

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(number)
+    i=1
+    huh = []
+    goo = [*1..number].map{|i| i}
+    while i <= goo.length do
+        huh << number.remainder(i)
+        i += 1
+    end
+    what = huh.select{|i| i === 0 }
+    if what.length == 2
+        true
+    else
+        false
+    end
+end
